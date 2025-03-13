49282b361a4cfd698d50220703be15e0e31c4561
Dancharike <145747723+Dancharike@users.noreply.github.com>
2025-03-13 15:35:42 +0200

    Initial commit

---
+++ src/main/java/lt/viko/eif/denis/kladijev/marshall/MarshallAndUnmarshallApplication.java
@@ -0,0 +1,14 @@
+package lt.viko.eif.denis.kladijev.marshall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarshallAndUnmarshallApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarshallAndUnmarshallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/viko/eif/denis/kladijev/marshall/MarshallAndUnmarshallApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.denis.kladijev.marshall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarshallAndUnmarshallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
